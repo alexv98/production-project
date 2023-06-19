ced87c06972fd8cb1287c4e55e2c8e293d5e7f39
Alexander <mr.vasilev9810@yandex.ru>
2023-06-19 21:38:15 +0300

    add modal, portal, add into navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
+import React, { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,9 +12,27 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
 
+  const [isAuth, setIsAuth] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuth((prevState) => !prevState);
+  }, []);
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links} />
+      <div className={cls.links}>
+        <Button
+          theme={ButtonTheme.CLEAR_INVERTED}
+          className={cls.link}
+          onClick={onToggleModal}
+        >
+          {t('Войти')}
+        </Button>
+      </div>
+      <Modal isOpen={isAuth} onClose={onToggleModal}>
+        {/* eslint-disable-next-line max-len */}
+        {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi illum laborum laudantium magni, natus quae? Animi impedit iure modi nesciunt vel voluptatum. Ad facilis hic magni nesciunt officia repellat.')}
+      </Modal>
     </div>
   );
 };
