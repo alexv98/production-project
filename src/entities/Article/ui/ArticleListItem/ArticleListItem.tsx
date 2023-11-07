e5934e977235fedd694b85c763d384e0c667fd99
alexv98 <mr.vasilev9810@yandex.ru>
2023-11-07 09:04:59 +0300

    add AppImage, fallback, error, errorFallback

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -19,6 +19,8 @@ import {
 } from '../../model/types/article';
 import cls from './ArticleListItem.module.scss';
 import { getRouteArticleDetails } from '@/shared/const/router';
+import { AppImage } from '@/shared/ui/AppImage/AppImage';
+import { Skeleton } from '@/shared/ui/Skeleton';
 
 interface ArticleListItemProps {
   className?: string;
@@ -56,7 +58,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
           className={cls.card}
         >
           <div className={cls.imageWrapper}>
-            <img src={article.img} alt="article" className={cls.img} />
+            <AppImage
+              src={article.img}
+              alt="article"
+              className={cls.img}
+              fallback={<Skeleton width={200} height={200} />}
+            />
             <Text text={article.createdAt} className={cls.date} />
           </div>
           <div className={cls.infoWrapper}>
@@ -83,7 +90,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
           </div>
           <Text title={article.title} className={cls.title} />
           {types}
-          <img src={article.img} alt={article.title} className={cls.img} />
+          <AppImage
+            src={article.img}
+            alt={article.title}
+            className={cls.img}
+            fallback={<Skeleton width="100%" height={250} />}
+          />
           {
             textBlock && (
               <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
