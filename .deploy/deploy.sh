48acce0507536276b2c8468e8bff2adc33e8b287
alexv98 <mr.vasilev9810@yandex.ru>
2023-11-21 15:11:43 +0300

    add script for deploy

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,4 +1,5 @@
 cd ~/production-project
 npm run build:prod
-rm -rf ~/..var/www/production-project/html
-mv ~/production-project/build ~/../var/www/production-project/html
+
+rm -rf /var/www/production-project/html
+mv /root/production-project/build /var/www/production-project/html
