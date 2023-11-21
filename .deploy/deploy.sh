934706a42080b969c18c8b1cc0a88657b74580a8
alexv98 <mr.vasilev9810@yandex.ru>
2023-11-21 14:58:16 +0300

    add script for deploy

---
+++ .deploy/deploy.sh
@@ -0,0 +1,4 @@
+cd ~/production-project
+npm run build:prod
+rm -rf ~/..var/www/production-project/html
+mv ~/production-project/build ~/../var/www/production-project/html
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -5,7 +5,7 @@ import { Page } from '@/widgets/Page';
 const MainPage = () => {
   const { t } = useTranslation();
 
-  return <Page data-testid="MainPage">{t('Главная страница')}</Page>;
+  return <Page data-testid="MainPage">{t('Главная страница (test)')}</Page>;
 };
 
 export default MainPage;
